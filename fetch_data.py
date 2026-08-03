4be9e4f08c9d451b467f859171b9ad64e22bd82a
angelinaharis97 <angelinaharis97@gmail.com>
2026-08-03 16:05:50 +0500

    Day 2: added weather data (temp, humidity, wind) alongside pollution data

--- fetch_data.py
+++ fetch_data.py
@@ -8,9 +8,18 @@ API_KEY = os.getenv("OPENWEATHER_API_KEY")
 LAT = 24.8607
 LON = 67.0011
 
-url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={LAT}&lon={LON}&appid={API_KEY}"
+# Call 1: Air pollution data (what you already had)
+pollution_url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={LAT}&lon={LON}&appid={API_KEY}"
+pollution_response = requests.get(pollution_url)
+pollution_data = pollution_response.json()
 
-response = requests.get(url)
-data = response.json()
+# Call 2: Weather data (new)
+weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric"
+weather_response = requests.get(weather_url)
+weather_data = weather_response.json()
 
-print(data)
+print("POLLUTION DATA:")
+print(pollution_data)
+
+print("\nWEATHER DATA:")
+print(weather_data)
